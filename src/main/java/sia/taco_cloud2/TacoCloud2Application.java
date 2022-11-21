befb984d9119a6ad4b095442a58da0e5188ed105
mohammadolama <rastegar123456789@gmail.com>
2022-11-21 11:04:11 +0330

    chapter 2 edited

--- src/main/java/sia/taco_cloud2/TacoCloud2Application.java
+++ src/main/java/sia/taco_cloud2/TacoCloud2Application.java
@@ -4,17 +4,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import sia.taco_cloud2.web.WebConfig;
 
 @SpringBootApplication
-public class TacoCloud2Application implements WebMvcConfigurer {
+public class TacoCloud2Application extends WebConfig {
 
     public static void main(String[] args) {
         SpringApplication.run(TacoCloud2Application.class, args);
-    }
-
-    @Override
-    public void addViewControllers(ViewControllerRegistry registry) {
-        registry.addViewController("/").setViewName("home");
     }
 
 }
