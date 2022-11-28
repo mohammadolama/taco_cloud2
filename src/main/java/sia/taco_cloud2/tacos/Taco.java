d7e5968697e8d55f67de504974ea3b347e7244aa
mohammadolama <rastegar123456789@gmail.com>
2022-11-28 19:22:45 +0330

    chapter 7: Creating some rest Api2.

--- src/main/java/sia/taco_cloud2/tacos/Taco.java
+++ src/main/java/sia/taco_cloud2/tacos/Taco.java
@@ -2,6 +2,8 @@ package sia.taco_cloud2.tacos;
 
 import lombok.Data;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.rest.core.annotation.RestResource;
 
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
@@ -11,11 +13,13 @@ import java.util.List;
 import java.util.UUID;
 
 @Data
-
+@Document
+@RestResource(rel = "tacos", path = "tacos")
 public class Taco {
 
     @Id
     private String id;
+
 
     @NotNull
     @Size(min = 5, message = "Name must be at least 5 characters long")
@@ -23,7 +27,7 @@ public class Taco {
 
     private Date createdAt = new Date();
 
-    @Size(min=1, message="You must choose at least 1 ingredient")
+    @Size(min = 1, message = "You must choose at least 1 ingredient")
     private List<Ingredient> ingredients = new ArrayList<>();
 
     public void addIngredient(Ingredient ingredient) {
